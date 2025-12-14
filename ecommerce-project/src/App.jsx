68b4d29ca20be35aef8649422336a7fb7f41e460
E.Dhanush <dhanushenugu22052006@gmail.com>
2025-12-14 19:45:28 +0530

    Add the starting files

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,22 +1,35 @@
-import { Routes,Route } from 'react-router'
-import { HomePage } from './pages/HomePage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test checkout page</div>} />
-    </Routes>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
 export default App
-
-
-// Routes tells React all the pages that are in our website
-// To add a page to our website: we use Route, Route = a page
-// path tells the url path of the page,url means uniform resource locator 
-// element tells = wehich element or component to display
-// path="/" means index in shortcut
-// of there is anything inside a componenet we can use self closing tag
