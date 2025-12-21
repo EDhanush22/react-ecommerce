e63c48ca5cc61f48de257e57d611a1243efd2fbb
E.Dhanush <dhanushenugu22052006@gmail.com>
2025-12-21 19:23:36 +0530

    Use beforeEach to remove duplication

--- ecommerce-project/src/pages/orders/Product.test.jsx
+++ ecommerce-project/src/pages/orders/Product.test.jsx
@@ -1,16 +1,23 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react"; // renders a component in a fake webpage,screen checks the fake web page
-import userEvent from '@testing-library/user-event'; 
+import userEvent from "@testing-library/user-event";
 import axios from "axios"; // Fake version
 import { Product } from "../home/Product";
 
-vi.mock('axios'); // mocks the entire axios package,when we click addToCartButton to we access the fake version of axios
+vi.mock("axios"); // mocks the entire axios package,when we click addToCartButton to we access the fake version of axios
 // but we can test the axios.post and the values given to axios,for that we have to import the axios
 
 // The following is a test suite
 describe("Product component", () => {
-  it("displays the product details correctly", () => {
-    const product = {
+  let product;
+  // we get loadCart from the backend but we can't contect backend to test,so we create mock test means creating a fake version,for that we have to import vi
+  let loadCart; // vi,fn creates a fake function that dosen't do anything, this is called mock
+
+  beforeEach(() => { // It is a Test Hook
+    // beforeEach() is a test lifecycle hook used in automated testing to run setup code before every test case in a test suite.
+    // beforeEach() runs before every it() test inside a describe() block.
+    product = {
+      // We are placing this outside the function so it can be accessed form anywhere,it can be accessed in both tests
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -22,9 +29,10 @@ describe("Product component", () => {
       keywords: ["socks", "sports", "apparel"],
     };
 
-    // we get loadCart from the backend but we can't contect backend to test,so we create mock test means creating a fake version,for that we have to import vi
-    const loadCart = vi.fn(); // vi,fn creates a fake function that dosen't do anything, this is called mock
+    loadCart = vi.fn();
+  });
 
+  it("displays the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />); // We have to pass the props as we are passing these props to Product component
 
     // For Product name
@@ -53,36 +61,19 @@ describe("Product component", () => {
   });
 
   it("adds a product to the cart", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    const loadCart = vi.fn();
- 
     render(<Product product={product} loadCart={loadCart} />);
 
     const user = userEvent.setup();
-    const addToCartButton = screen.getByTestId('add-to-cart-button');
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
     await user.click(addToCartButton); //it is asynchronous code
     // After clicking the button it sends the rquest to the backend,in our tests we should not contact a reaal backend
 
-    expect(axios.post).toHaveBeenCalledWith(
-      '/api/cart-items',
-      {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1
-      }
-    );
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    });
+
     expect(loadCart).toHaveBeenCalled(); // Check whether our code called loadCart
   });
 });
-
 // to check we create fake webpage,we use screen for that
